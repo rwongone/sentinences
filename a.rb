353c0d97c198868689a155516d6eaa1cd65329de
Richard Wong <rk2wong@uwaterloo.ca>
2014-01-20 20:45:59 +0000

    cleanup

--- a.rb
+++
@@ -1,4 +0,0 @@
-string1 = "abc"
-if "abc" == string1
-  puts 'they are equal'
-end
--- structs.rb
+++
@@ -1,7 +0,0 @@
-class Link
-  def initialize(src, dest)
-    @src = src
-    @dest = dest
-    @strength = 1
-  end
-end
